eef8448b250e7389d3c931df61e79b0671d0d784
brandonsams <brandonkeithsams@gmail.com>
2023-12-07 13:09:59 -0700

    Completed Day7Part2

--- Day07/Day7_2.py
+++ Day07/Day7_2.py
@@ -1,19 +1,18 @@
 from enum import Enum
 from tqdm import tqdm
 
-cards_by_rank = ["A", "K", "Q", "J", "T",
-                 "9", "8", "7", "6", "5", "4", "3", "2"]
+cards_by_rank = ["A", "K", "Q", "T", "9",
+                 "8", "7", "6", "5", "4", "3", "2", "J"]
 
-HandType = Enum("HandType",
-                [
-                    "Five_of_a_kind",
-                    "Four_of_a_kind",
-                    "Full_House",
-                    "Three_of_a_kind",
-                    "Two_pair",
-                    "One_pair",
-                    "High_card"
-                ])
+HandType = Enum("HandType", [
+    "Five_of_a_kind",
+    "Four_of_a_kind",
+    "Full_House",
+    "Three_of_a_kind",
+    "Two_pair",
+    "One_pair",
+    "High_card"
+])
 
 
 class CamelCardsHand:
@@ -23,6 +22,13 @@ class CamelCardsHand:
         self.card_counts = dict()
         for card in hand:
             self.card_counts[card] = self.card_counts.get(card, 0) + 1
+        joker_bonus = self.card_counts.pop("J", 0)
+        frequent_card = None
+        # Special case handling for 5 Jokers
+        if self.hand == "JJJJJ":
+            self.card_counts["A"] = 0
+        frequent_card = max(self.card_counts, key=self.card_counts.get)
+        self.card_counts[frequent_card] += joker_bonus
 
     def get_hand_type(self):
         rv = None
@@ -50,27 +56,27 @@ class CamelCardsHand:
         return rv
 
 
-def hand_a_better_than_hand_b(hand_a: CamelCardsHand, hand_b: CamelCardsHand):
+def hand_a_better_than_hand_b(hand_a: CamelCardsHand, hand_b: CamelCardsHand) -> bool:
     if hand_a.get_hand_type().value < hand_b.get_hand_type().value:
         return True
     if hand_a.get_hand_type().value > hand_b.get_hand_type().value:
         return False
 
-    for i in range(5):
-        a_rank = cards_by_rank.index(hand_a.hand[i])
-        b_rank = cards_by_rank.index(hand_b.hand[i])
+    for card_index in range(5):
+        a_rank = cards_by_rank.index(hand_a.hand[card_index])
+        b_rank = cards_by_rank.index(hand_b.hand[card_index])
         if a_rank < b_rank:
             return True
         if a_rank > b_rank:
             return False
 
-    # They must be equal at this point
+    # They must be equal at this point, so a can't be better than b
     return False
 
 
 camel_cards_hands = []
 
-# fname = "./Day07/example.txt"
+fname = "./Day07/example.txt"
 fname = "./Day07/input.txt"
 with open(fname) as f:
     for line in tqdm(f.readlines()):
